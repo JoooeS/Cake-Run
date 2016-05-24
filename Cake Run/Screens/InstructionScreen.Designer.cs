cd6291234621e83246c33777c476d03ecdff2a1c
JoooeS <joe.v.soukphachanh@gmail.com>
2016-05-24 09:43:53 -0400

    Initial commit

---
+++ Cake Run/Screens/InstructionScreen.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace Cake_Run
+{
+    partial class InstructionScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelTitleDescription = new System.Windows.Forms.Label();
+            this.labelTitleInstruction = new System.Windows.Forms.Label();
+            this.labelDescDetails = new System.Windows.Forms.Label();
+            this.labelInstrDetails = new System.Windows.Forms.Label();
+            this.indicator1 = new System.Windows.Forms.Label();
+            this.indicator2 = new System.Windows.Forms.Label();
+            this.indicator4 = new System.Windows.Forms.Label();
+            this.indicator3 = new System.Windows.Forms.Label();
+            this.labelFiller = new System.Windows.Forms.Label();
+            this.labelBlack = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelTitleDescription
+            // 
+            this.labelTitleDescription.BackColor = System.Drawing.Color.Aqua;
+            this.labelTitleDescription.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.labelTitleDescription.Location = new System.Drawing.Point(19, 59);
+            this.labelTitleDescription.Name = "labelTitleDescription";
+            this.labelTitleDescription.Size = new System.Drawing.Size(380, 80);
+            this.labelTitleDescription.TabIndex = 0;
+            this.labelTitleDescription.Text = "Description\r\n";
+            this.labelTitleDescription.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelTitleInstruction
+            // 
+            this.labelTitleInstruction.BackColor = System.Drawing.Color.Green;
+            this.labelTitleInstruction.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.labelTitleInstruction.Location = new System.Drawing.Point(401, 59);
+            this.labelTitleInstruction.Name = "labelTitleInstruction";
+            this.labelTitleInstruction.Size = new System.Drawing.Size(380, 80);
+            this.labelTitleInstruction.TabIndex = 1;
+            this.labelTitleInstruction.Text = "Instructions";
+            this.labelTitleInstruction.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelDescDetails
+            // 
+            this.labelDescDetails.BackColor = System.Drawing.Color.Black;
+            this.labelDescDetails.ForeColor = System.Drawing.SystemColors.Control;
+            this.labelDescDetails.Location = new System.Drawing.Point(19, 141);
+            this.labelDescDetails.Name = "labelDescDetails";
+            this.labelDescDetails.Size = new System.Drawing.Size(380, 448);
+            this.labelDescDetails.TabIndex = 2;
+            this.labelDescDetails.Text = "blah blah";
+            this.labelDescDetails.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelInstrDetails
+            // 
+            this.labelInstrDetails.BackColor = System.Drawing.Color.Black;
+            this.labelInstrDetails.ForeColor = System.Drawing.SystemColors.Control;
+            this.labelInstrDetails.Location = new System.Drawing.Point(401, 141);
+            this.labelInstrDetails.Name = "labelInstrDetails";
+            this.labelInstrDetails.Size = new System.Drawing.Size(380, 448);
+            this.labelInstrDetails.TabIndex = 5;
+            this.labelInstrDetails.Text = "blah blah";
+            this.labelInstrDetails.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // indicator1
+            // 
+            this.indicator1.BackColor = System.Drawing.Color.Red;
+            this.indicator1.ForeColor = System.Drawing.SystemColors.Control;
+            this.indicator1.Location = new System.Drawing.Point(57, 591);
+            this.indicator1.Name = "indicator1";
+            this.indicator1.Size = new System.Drawing.Size(80, 80);
+            this.indicator1.TabIndex = 6;
+            this.indicator1.Text = "Player 1\r\n(Press x)";
+            this.indicator1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // indicator2
+            // 
+            this.indicator2.BackColor = System.Drawing.Color.Red;
+            this.indicator2.ForeColor = System.Drawing.SystemColors.Control;
+            this.indicator2.Location = new System.Drawing.Point(265, 591);
+            this.indicator2.Name = "indicator2";
+            this.indicator2.Size = new System.Drawing.Size(80, 80);
+            this.indicator2.TabIndex = 7;
+            this.indicator2.Text = "Player 2\r\n(Press m)\r\n";
+            this.indicator2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // indicator4
+            // 
+            this.indicator4.BackColor = System.Drawing.Color.Red;
+            this.indicator4.ForeColor = System.Drawing.SystemColors.Control;
+            this.indicator4.Location = new System.Drawing.Point(667, 591);
+            this.indicator4.Name = "indicator4";
+            this.indicator4.Size = new System.Drawing.Size(80, 80);
+            this.indicator4.TabIndex = 8;
+            this.indicator4.Text = "FOR LATER";
+            this.indicator4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // indicator3
+            // 
+            this.indicator3.BackColor = System.Drawing.Color.Red;
+            this.indicator3.ForeColor = System.Drawing.SystemColors.Control;
+            this.indicator3.Location = new System.Drawing.Point(473, 591);
+            this.indicator3.Name = "indicator3";
+            this.indicator3.Size = new System.Drawing.Size(80, 80);
+            this.indicator3.TabIndex = 9;
+            this.indicator3.Text = "FOR LATER";
+            this.indicator3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelFiller
+            // 
+            this.labelFiller.BackColor = System.Drawing.Color.Aqua;
+            this.labelFiller.ForeColor = System.Drawing.SystemColors.Control;
+            this.labelFiller.Location = new System.Drawing.Point(19, 589);
+            this.labelFiller.Name = "labelFiller";
+            this.labelFiller.Size = new System.Drawing.Size(762, 84);
+            this.labelFiller.TabIndex = 10;
+            this.labelFiller.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelBlack
+            // 
+            this.labelBlack.Location = new System.Drawing.Point(0, 0);
+            this.labelBlack.Name = "labelBlack";
+            this.labelBlack.Size = new System.Drawing.Size(800, 772);
+            this.labelBlack.TabIndex = 11;
+            this.labelBlack.Visible = false;
+            // 
+            // InstructionScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.BackgroundImage = global::Cake_Run.Properties.Resources.BKGInstructionScreen;
+            this.ClientSize = new System.Drawing.Size(800, 772);
+            this.Controls.Add(this.indicator3);
+            this.Controls.Add(this.indicator4);
+            this.Controls.Add(this.indicator2);
+            this.Controls.Add(this.indicator1);
+            this.Controls.Add(this.labelInstrDetails);
+            this.Controls.Add(this.labelDescDetails);
+            this.Controls.Add(this.labelTitleInstruction);
+            this.Controls.Add(this.labelTitleDescription);
+            this.Controls.Add(this.labelFiller);
+            this.Controls.Add(this.labelBlack);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "InstructionScreen";
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.InstructionScreen_KeyUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelTitleDescription;
+        private System.Windows.Forms.Label labelTitleInstruction;
+        private System.Windows.Forms.Label labelDescDetails;
+        private System.Windows.Forms.Label labelInstrDetails;
+        private System.Windows.Forms.Label indicator1;
+        private System.Windows.Forms.Label indicator2;
+        private System.Windows.Forms.Label indicator4;
+        private System.Windows.Forms.Label indicator3;
+        private System.Windows.Forms.Label labelFiller;
+        private System.Windows.Forms.Label labelBlack;
+    }
+}
+
---
+++ Cake Run/Screens/InstructionScreen.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+using System.Threading;
+
+namespace Cake_Run
+{
+    public partial class InstructionScreen : Form
+    {
+        const int PLAYERS_NEEDED_TO_BE_READY = 2;
+        int playersCurrentlyReady = 0;
+
+        public InstructionScreen()
+        {
+            InitializeComponent();
+
+            labelTitleDescription.BackColor = Color.FromArgb(240, 255, 0, 255);
+            labelTitleInstruction.BackColor = Color.FromArgb(240, 0, 255, 0);
+            labelFiller.BackColor = Color.FromArgb(240, 0, 80, 80);
+            labelDescDetails.BackColor = labelInstrDetails.BackColor = Color.FromArgb(240, 0, 0, 0);
+            labelBlack.BackColor = Color.FromArgb(0, 0, 0, 0);
+
+            GraphicsPath circlePath = new GraphicsPath();
+            circlePath.AddEllipse(0, 0, 80, 80);
+
+            indicator1.Region = new Region(circlePath);
+            indicator2.Region = new Region(circlePath);
+            indicator3.Region = new Region(circlePath);
+            indicator4.Region = new Region(circlePath);
+        }
+
+        private void InstructionScreen_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.X:
+                    playersCurrentlyReady++;
+                    indicator1.BackColor = Color.Green;
+                    Refresh();
+                    Check();
+                    break;
+                case Keys.M:
+                    playersCurrentlyReady++;
+                    indicator2.BackColor = Color.Green;
+                    Refresh();
+                    Check();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public void Check()
+        {
+            if (playersCurrentlyReady == PLAYERS_NEEDED_TO_BE_READY)
+            {
+                labelBlack.Visible = true;
+                labelBlack.BackColor = Color.FromArgb(0, 0, 0, 0);
+                labelBlack.BringToFront();
+                Refresh();
+
+                for (int i = 0; i < 256; i+= 8)
+                {
+                    labelBlack.BackColor = Color.FromArgb(i, 0, 0, 0);
+                    labelBlack.Refresh();
+                    Thread.Sleep(10);
+                }
+
+
+                GameScreen gs = new GameScreen();
+                this.Controls.Add(gs);
+                gs.BringToFront();
+            }
+        }
+    }
+}
